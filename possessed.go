86ba61fec2450d1e0c311c010b8fad5660ea33af
Robert Daniels <rob.daniels3@gmail.com>
2025-06-18 23:02:13 +0100

    🧛 Possessed rewrite 2025-06-18 23:02:14.466587664 +0100 BST m=+0.001937998

--- possessed.go
+++ possessed.go
@@ -180,6 +180,8 @@ func main() {
 // 😱 Possessed run at 2025-06-18 23:02:13.877633796 +0100 BST m=+0.000023700
 
 	lines = append(lines[:insertionPoint], append([]string{incantation}, lines[insertionPoint:]...)...)
+// 😱 Possessed run at 2025-06-18 23:02:14.464697786 +0100 BST m=+0.000048110
+
 // 😱 Possessed run at 2025-06-18 23:02:14.391289943 +0100 BST m=+0.000042700
 
 // 😱 Possessed run at 2025-06-18 22:57:13.043804063 +0100 BST m=+0.000041480
