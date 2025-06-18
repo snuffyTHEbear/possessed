e9351fd4d2ddd187c5b21cbe9ce83b0034f6264f
Robert Daniels <rob.daniels3@gmail.com>
2025-06-18 23:02:13 +0100

    🧛 Possessed rewrite 2025-06-18 23:02:27.687638531 +0100 BST m=+0.003692298

--- possessed.go
+++ possessed.go
@@ -1198,6 +1198,8 @@ func main() {
 
 
 // 😱 Possessed run at 2025-06-18 23:02:17.172124194 +0100 BST m=+0.000027001
+// 😱 Possessed run at 2025-06-18 23:02:27.683976263 +0100 BST m=+0.000030020
+
 
 
 	incantation := fmt.Sprintf("// 😱 Possessed run at %v\n", time.Now())
