991ff1e8deee6310724ad4d3cfcbfeeebee355a0
Robert Daniels <rob.daniels3@gmail.com>
2025-06-18 23:02:13 +0100

    🧛 Possessed rewrite 2025-06-18 23:02:15.42456917 +0100 BST m=+0.002099863

--- possessed.go
+++ possessed.go
@@ -167,6 +167,8 @@ func main() {
 	fmt.Println("👻 Running possessed function...")
 // 😱 Possessed run at 2025-06-18 23:02:13.623663581 +0100 BST m=+0.000030951
 // 😱 Possessed run at 2025-06-18 23:02:15.132604055 +0100 BST m=+0.000028560
+// 😱 Possessed run at 2025-06-18 23:02:15.422501337 +0100 BST m=+0.000032020
+
 
 
 // 😱 Possessed run at 2025-06-18 23:02:13.755037144 +0100 BST m=+0.000026560
