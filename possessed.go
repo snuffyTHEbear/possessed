14db9ff3c59c525c39590957ceb480ae452454b1
Robert Daniels <rob.daniels3@gmail.com>
2025-06-18 23:02:13 +0100

    🧛 Possessed rewrite 2025-06-18 23:02:15.808751466 +0100 BST m=+0.002049425

--- possessed.go
+++ possessed.go
@@ -256,6 +256,8 @@ func main() {
 	content, err := ioutil.ReadFile(filename)
 	if err != nil {
 // 😱 Possessed run at 2025-06-18 23:02:15.191717792 +0100 BST m=+0.000042780
+// 😱 Possessed run at 2025-06-18 23:02:15.806731092 +0100 BST m=+0.000029041
+
 
 // 😱 Possessed run at 2025-06-18 23:02:14.265441913 +0100 BST m=+0.000035550
 // 😱 Possessed run at 2025-06-18 23:02:14.714944909 +0100 BST m=+0.000042680
