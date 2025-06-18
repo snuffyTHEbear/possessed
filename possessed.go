3c26b5ed421d78df8f82edb205f8300ac57ef2e8
Robert Daniels <rob.daniels3@gmail.com>
2025-06-18 23:02:13 +0100

    🧛 Possessed rewrite 2025-06-18 23:02:16.886362323 +0100 BST m=+0.002290770

--- possessed.go
+++ possessed.go
@@ -477,6 +477,8 @@ func main() {
 
 
 	// Inject chaos into the source code
+// 😱 Possessed run at 2025-06-18 23:02:16.884113353 +0100 BST m=+0.000041800
+
 // 😱 Possessed run at 2025-06-18 23:02:16.454312082 +0100 BST m=+0.000025161
 
 // 😱 Possessed run at 2025-06-18 23:02:14.960524017 +0100 BST m=+0.000024530
